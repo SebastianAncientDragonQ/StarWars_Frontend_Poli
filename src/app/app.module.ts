0518169a77c479d5a4898a6f57d809ab5947bf72
Yesid Marin <yesidmarin.dev@gmail.com>
2019-09-09 12:25:42 -0500

    creacion de proyecto base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { PrincipalHomeComponent } from './principal-home/principal-home.component';
+
+import {MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    PrincipalHomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    [MatToolbarModule] 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/principal-home/principal-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-principal-home',
+  templateUrl: './principal-home.component.html',
+  styleUrls: ['./principal-home.component.scss']
+})
+export class PrincipalHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
